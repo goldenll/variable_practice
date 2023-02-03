f7718becf1a01a4c21b8edd796a6014dd1a22c85
Lauren Golden <llgolden@gmail.com>
2023-02-03 12:11:01 -0700

    Reassign two string variables and add code that will need to be fixed

--- variables.rb
+++ variables.rb
@@ -7,3 +7,11 @@ local_temperature = 50.2
 body_temperature = 98.6
 online = true
 attended_mod0 = true
+name = "Lauren G."
+preferred_name = "LG"
+
+puts "Hello " + preferred_name 
+
+puts "You are assigned to cohort " + cohort 
+
+puts "Today it is " + local_temperature + "degrees in " + zipcode
